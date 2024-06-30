c99e37b5325da4dbadfc11b4748353c1ef89fff3
ekarkat <ekarkat@gmail.com>
2024-06-30 05:11:20 +0100

    small change

--- dashapp.py
+++ dashapp.py
@@ -1,13 +1,11 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
 
+
 app = Dash(__name__)
+
 
 colors = {
     'background': 'white',
